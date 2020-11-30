f5f70a215b73126b3775f5ffb9383a3b9b1e0379
Rajasegar <rajasegar.c@gmail.com>
2020-11-30 12:11:00 +0530

    chore: first commit

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+/* globals process */
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import image from '@rollup/plugin-image';
+import css from 'rollup-plugin-css-only';
+import babel from '@rollup/plugin-babel';
+import serve from 'rollup-plugin-serve';
+import { terser } from 'rollup-plugin-terser';
+
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const plugins = [
+  nodeResolve(),
+  commonjs({
+    include: 'node_modules/**',
+  }),
+  image(),
+  css({ output: 'bundle.css' }),
+  babel(),
+  !IS_PRODUCTION &&
+    serve({
+      open: true,
+      port: 8080,
+      contentBase: 'public',
+    }),
+  IS_PRODUCTION && terser(),
+];
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: 'public/dist/bundle.js',
+  },
+  plugins,
+};
